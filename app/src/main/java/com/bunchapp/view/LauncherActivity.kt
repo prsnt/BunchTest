fd1cb0c723f2dc57a8dbe17621f0e137e42e645f
Prashant Thakkar <prashant.thakkar1593@gmail.com>
2023-08-30 01:14:09 -0400

    commit

--- app/src/main/java/com/bunchapp/view/LauncherActivity.kt
+++ app/src/main/java/com/bunchapp/view/LauncherActivity.kt
@@ -1,13 +1,15 @@
 package com.bunchapp.view
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.bunchapp.R
 import com.bunchapp.databinding.ActivityLauncherBinding
-import com.bunchapp.databinding.ActivityListingBinding
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class LauncherActivity : AppCompatActivity(), View.OnClickListener {
     private var binding: ActivityLauncherBinding? = null
@@ -21,6 +23,11 @@ class LauncherActivity : AppCompatActivity(), View.OnClickListener {
     private fun init() {
         binding?.btnEmpAdd?.setOnClickListener(this)
         binding?.btnEmpList?.setOnClickListener(this)
+
+        AppCenter.start(
+            application, "7d3cfe6c-0a67-4ae6-81a8-3433b542bbbb",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 
     override fun onClick(v: View?) {
